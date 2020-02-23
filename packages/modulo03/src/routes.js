5495df58dbb74c46e2fd57321529794131793044
mauricio-andre <mauricio.r.andre@gmail.com>
2020-02-23 18:22:01 -0300

    Modulo03 - controller para a listagem de agendamentos dos providers

--- packages/modulo03/src/routes.js
+++ packages/modulo03/src/routes.js
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import multerConfig from './config/multer';
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
-import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const uploads = multer(multerConfig);
@@ -24,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', uploads.single('file'), FileController.store);
 
