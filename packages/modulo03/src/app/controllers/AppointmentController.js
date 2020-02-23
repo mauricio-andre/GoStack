439e6fcd526f3f52a2753122fd6049e7df89592c
mauricio-andre <mauricio.r.andre@gmail.com>
2020-02-23 16:25:59 -0300

    Modulo03 - Sistema de paginação da listagem de appointments, do usuário

--- packages/modulo03/src/app/controllers/AppointmentController.js
+++ packages/modulo03/src/app/controllers/AppointmentController.js
@@ -6,10 +6,13 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointment = await Appointment.findAll({
       where: { userId: req.userId, canceledAt: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
