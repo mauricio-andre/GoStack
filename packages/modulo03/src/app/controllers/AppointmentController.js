091818f75ccbf773a9cb05a61b42c094f3843ec4
mauricio-andre <mauricio.r.andre@gmail.com>
2020-02-26 15:44:10 -0300

    Modulo03 - rota para cancelar os appointments

--- packages/modulo03/src/app/controllers/AppointmentController.js
+++ packages/modulo03/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -113,6 +113,29 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para o ${formatDate}`,
       user: providerId,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.userId !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateSubHours = subHours(appointment.date, 2);
+    if (isBefore(dateSubHours, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance.',
+      });
+    }
+
+    appointment.canceledAt = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
