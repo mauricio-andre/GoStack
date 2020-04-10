7bfe1ab532458f6a087b0aeb43fbe1e289c0df27
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-04-10 18:42:26 -0300

    Modulo05 - Exibição das issues de um repositório

--- packages/Modulo05/src/pages/Repository/index.js
+++ packages/Modulo05/src/pages/Repository/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 
-import { Loading, Ownner } from './styles';
+import { Loading, Ownner, IssueList } from './styles';
 import Container from '../../components/Container';
 
 class Repository extends Component {
@@ -51,6 +51,23 @@ class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Ownner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
