ca0edd638fc7c9deebabf793404120a6f4856286
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-04-10 15:59:53 -0300

    Modulo05 - Adicionando repositórios

--- packages/Modulo05/src/pages/Main/index.js
+++ packages/Modulo05/src/pages/Main/index.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import React, { Component } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithub />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  constructor() {
+    super();
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    const { newRepo, repositories } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithub />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default Main;
