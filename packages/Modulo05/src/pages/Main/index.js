1dd028aa4674aff3821b31c81606dc990e6854b9
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-04-10 15:10:01 -0300

    Modulo05 - Estilizando a página Main

--- packages/Modulo05/src/pages/Main/index.js
+++ packages/Modulo05/src/pages/Main/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
