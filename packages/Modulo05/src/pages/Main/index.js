5d2ac05cee63fcb2de67d38e87a8c2434a59e897
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-04-10 16:47:37 -0300

    Modulo05 - Navegação de rotas cm parametros

--- packages/Modulo05/src/pages/Main/index.js
+++ packages/Modulo05/src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
@@ -83,7 +84,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href={`#${repository.name}`}>Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
