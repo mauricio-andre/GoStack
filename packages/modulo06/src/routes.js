1caae03d3be204f1df6b2eab2b877ae0d6dd638a
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-04-22 20:58:43 -0300

    Modulo06 - Configuração do react navigator

---
+++ packages/modulo06/src/routes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+export default function Routes() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerBackTitleVisible: false,
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: '#7159c1' },
+        }}
+      >
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{
+            title: 'Main',
+          }}
+        />
+        <Stack.Screen
+          name="User"
+          component={User}
+          options={{
+            title: 'Usuário',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
