59b69aafa9a3f8ba95b5223a758ae1a7b11edbe9
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-01 18:24:55 -0300

    Modulo06 - Usado lib para renomear o projeto de modulo06 para firstReactApp

--- packages/modulo06/android/app/src/main/java/com/firstreactapp/MainActivity.java
+++ packages/modulo06/android/app/src/main/java/com/firstreactapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.modulo06;
+package com.firstreactapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "modulo06";
+    return "firstReactApp";
   }
 }
