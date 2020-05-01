6842e7f9ec605450c0e11642d16d9053a4358122
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-01 16:53:28 -0300

    Modulo06 - Buscando dados complementares da api

--- packages/modulo06/src/pages/User/index.js
+++ packages/modulo06/src/pages/User/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-export default function User({ route }) {
-  console.tron.log(route.params.user);
-  return <View />;
+class User extends Component {
+  constructor({ navigation, route }) {
+    super();
+    this.state = {
+      stars: [],
+    };
+
+    navigation.setOptions({
+      title: route.params.user.name,
+    });
+  }
+
+  async componentDidMount() {
+    const { route } = this.props;
+    const { user } = route.params;
+
+    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    setOptions: PropTypes.func,
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      user: PropTypes.shape({
+        name: PropTypes.string,
+        login: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+};
+
+export default User;
