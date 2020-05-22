efa26522396161a91466a1a5a7cfa16b45fd4d90
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-22 20:06:38 -0300

    Modulo09 - Exibindo mensaens com toasts

--- packages/GoBarber/frontend/src/store/modules/auth/sagas.js
+++ packages/GoBarber/frontend/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -16,13 +17,14 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.log('usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
 
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique os seus dados');
     yield put(signFailure());
   }
 }
