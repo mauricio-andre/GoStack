2909ddcea523dbf92da23793e477b952c430e165
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-23 16:23:47 -0300

    Modulo09 - Função de logout

--- packages/GoBarber/frontend/src/pages/Profile/index.js
+++ packages/GoBarber/frontend/src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { Form, Input } from '@rocketseat/unform';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
@@ -13,6 +14,9 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -39,7 +43,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
