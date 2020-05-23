fb53a091b27133aadd45e7c266100251869d10ae
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-22 21:10:00 -0300

    Modulo09 - Configurando o header da aplicação

---
+++ packages/GoBarber/frontend/src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+import { ReactComponent as Logo } from '~/assets/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <Logo alt="GoBarber" fill="#7159C1" />
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Usuário</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Usuário"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
