380a78982b7d8f142012dadf487a177a1987234f
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-24 16:58:42 -0300

    Modulo10 - Conectando o redux

--- packages/GoBarber/mobile/src/pages/SignIn/index.js
+++ packages/GoBarber/mobile/src/pages/SignIn/index.js
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Background from '~/components/Background';
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import {
   Container,
@@ -17,8 +19,16 @@ import logo from '~/assets/logo.png';
 export default function SignIn() {
   const passwordRef = useRef();
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
-  function handleSubmit() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -33,6 +43,8 @@ export default function SignIn() {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -41,9 +53,13 @@ export default function SignIn() {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Acessar
+          </SubmitButton>
 
           <SignLink onPress={() => navigation.navigate('SignUp')}>
             <SignLinkText>Criar conta gratuita</SignLinkText>
--- packages/GoBarber/mobile/src/pages/SignUp/index.js
+++ packages/GoBarber/mobile/src/pages/SignUp/index.js
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Background from '~/components/Background';
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import {
   Container,
@@ -18,8 +20,17 @@ export default function SignUp() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const navigation = useNavigation();
+  const dispath = useDispatch();
 
-  function handleSubmit() {}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispath(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -32,6 +43,8 @@ export default function SignUp() {
             autoCapitalize="none"
             placeholder="Nome completo"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
           <FormInput
             icon="mail-outline"
@@ -41,6 +54,8 @@ export default function SignUp() {
             placeholder="Digite seu e-mail"
             ref={emailRef}
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -49,9 +64,13 @@ export default function SignUp() {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Cadastrar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Cadastrar
+          </SubmitButton>
 
           <SignLink onPress={() => navigation.navigate('SignIn')}>
             <SignLinkText>Já tenho uma conta</SignLinkText>
