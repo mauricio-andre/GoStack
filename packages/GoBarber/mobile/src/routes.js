ed1dd175c02e11236280a611a26027ec18771389
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-05-31 18:00:08 -0300

    Modulo10 - Criação da toda para o dashboard

--- packages/GoBarber/mobile/src/routes.js
+++ packages/GoBarber/mobile/src/routes.js
@@ -1,31 +1,48 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 function Routes() {
+  const signed = useSelector(state => state.auth.signed);
   const Stack = createStackNavigator();
+  // const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{
-            header: () => {},
-          }}
-          name="SignIn"
-          component={SignIn}
-        />
-        <Stack.Screen
-          options={{
-            header: () => {},
-          }}
-          name="SignUp"
-          component={SignUp}
-        />
-      </Stack.Navigator>
+      {!signed ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{
+              header: () => {},
+            }}
+            name="SignIn"
+            component={SignIn}
+          />
+          <Stack.Screen
+            options={{
+              header: () => {},
+            }}
+            name="SignUp"
+            component={SignUp}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator independent>
+          <Stack.Screen
+            options={{
+              header: () => {},
+            }}
+            name="Dashboard"
+            component={Dashboard}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
