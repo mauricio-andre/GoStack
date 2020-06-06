fa73d4613a0aaab1d991690bf333b00388002c26
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-06-06 16:02:32 -0300

    Modulo10 - Layout da listagem de agendamentos

---
+++ packages/GoBarber/mobile/src/components/Appointments/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointments() {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{ uri: 'https://api.adorable.io/avatar/50/rocketseat.png' }}
+        />
+        <Info>
+          <Name>Mauricio</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#f64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}
--- packages/GoBarber/mobile/src/pages/Dashboard/index.js
+++ packages/GoBarber/mobile/src/pages/Dashboard/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import Background from '~/components/Background';
-
+import Appointments from '~/components/Appointments';
 import BottomTabs from '~/components/BottomTabs';
+
+import { Container, Title, List } from './styles';
+
+const data = [1, 2, 3, 4, 5, 6];
 
 export default function Dashboard() {
   return (
     <Background>
-      <Text>Dashboard</Text>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointments data={item} />}
+        />
+      </Container>
       <BottomTabs />
     </Background>
   );
