d3215bfb366f1000ca7950f2256ac4d26874af40
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-06-06 16:40:12 -0300

    Modulo10 - Integrar listagem de agendamentos com api

--- packages/GoBarber/mobile/src/components/Appointments/index.js
+++ packages/GoBarber/mobile/src/components/Appointments/index.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointments() {
+export default function Appointments({ data, onCancel }) {
+  const dateParse = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
-          source={{ uri: 'https://api.adorable.io/avatar/50/rocketseat.png' }}
+          source={{
+            uri: data.provider.avatar.url
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
+          }}
         />
         <Info>
-          <Name>Mauricio</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParse}</Time>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+      {data.cancelable && !data.canceledAt && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
--- packages/GoBarber/mobile/src/pages/Dashboard/index.js
+++ packages/GoBarber/mobile/src/pages/Dashboard/index.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import api from '~/services/api';
 import Background from '~/components/Background';
 import Appointments from '~/components/Appointments';
 import BottomTabs from '~/components/BottomTabs';
 
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5, 6];
-
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceledAt: response.data.canceledAt,
+            }
+          : appointment
+      )
+    );
+  }
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={data}
-          keyExtractor={item => String(item)}
-          renderItem={({ item }) => <Appointments data={item} />}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointments onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
       <BottomTabs />
