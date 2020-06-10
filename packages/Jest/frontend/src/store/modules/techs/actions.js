66238720161ddae2ea36a107a7884c02e11cecc3
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-06-10 19:54:33 -0300

    Modulo12 - testando Sagas

--- packages/Jest/frontend/src/store/modules/techs/actions.js
+++ packages/Jest/frontend/src/store/modules/techs/actions.js
@@ -4,3 +4,16 @@ export function addTechs(tech) {
     payload: { tech },
   };
 }
+
+export function getTechsSuccess(data) {
+  return {
+    type: 'GET_TECHS_SUCCESS',
+    payload: { data },
+  };
+}
+
+export function getTechsFailure() {
+  return {
+    type: 'GET_TECHS_FAILURE',
+  };
+}
