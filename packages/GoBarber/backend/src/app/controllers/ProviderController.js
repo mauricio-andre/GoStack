11ae43e56ca505fa834a98a43a971426955384de
Mauricio Andre <mauricio.redmerski@ajmer.com.br>
2020-06-13 16:26:42 -0300

    Modulo15 - usando cache

--- packages/GoBarber/backend/src/app/controllers/ProviderController.js
+++ packages/GoBarber/backend/src/app/controllers/ProviderController.js
@@ -1,8 +1,16 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(req, res) {
+    const cached = await Cache.get('providers');
+
+    if (cached) {
+      return res.json(cached);
+    }
+
     const provider = await User.findAll({
       where: {
         provider: true,
@@ -17,7 +25,9 @@ class ProviderController {
       ],
     });
 
-    res.json(provider);
+    await Cache.set('providers', provider);
+
+    return res.json(provider);
   }
 }
 
